6fbe9abfe864d5bebc021a20e5568422374eb0d7
hanasiu <youryouu@naver.com>
2024-01-06 16:29:38 +0900

    Initial commit CardView isFaceUp

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by kimyu on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.mint )
+        .padding()
+        
+    }
+    
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        if isFaceUp {
+            ZStack(
+                content: {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundColor(.white)
+                    RoundedRectangle(cornerRadius: 12)
+                        .strokeBorder(lineWidth: 2)
+                    
+                    Text("🐳")
+                        .font(.largeTitle)
+                })
+        } else {
+            ZStack(
+                content: {
+                    RoundedRectangle(cornerRadius: 12)
+                })
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by kimyu on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
