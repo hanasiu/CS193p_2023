7813fccc4e65d5b628cfc530bd9f5ecae76c44bb
hanasiu <youryouu@naver.com>
2024-01-07 15:59:15 +0900

    test last

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by kimyu on 1/7/24.
+//  Created by kimyu on 1/6/24.
 //
 
 import SwiftUI
@@ -10,15 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
         }
+        .foregroundColor(.mint )
         .padding()
+        
+    }
+    
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        if isFaceUp {
+            ZStack(
+                content: {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundColor(.white)
+                    RoundedRectangle(cornerRadius: 12)
+                        .strokeBorder(lineWidth: 2)
+                    
+                    Text("🐳")
+                        .font(.largeTitle)
+                })
+        } else {
+            ZStack(
+                content: {
+                    RoundedRectangle(cornerRadius: 12)
+                })
+        }
+        
     }
 }
 
 #Preview {
-    ContentView()
-}
+    ContentView()}
