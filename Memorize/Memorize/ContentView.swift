1e7aaf0d7aab7a8585d837f9be549a402f6d9a9c
hanasiu <youryouu@naver.com>
2024-01-07 15:08:08 +0900

    xcode commit test

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             CardView(isFaceUp: true)
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
         }
         .foregroundColor(.mint )
