1ad99d2adfc8757572fcf7f28064ee28a1dc6583
hanasiu <youryouu@naver.com>
2024-01-24 14:36:01 +0900

    assignment 1

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by kimyu on 1/12/24.
+//
+
+import SwiftUI
+
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🐳", "🦀", "🐙", "🐠", "🦭", "🦈", "🦐", "🪼", "🐡", "🐟"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame(numberOfPairsOfCards: 10) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = EmojiMemoryGame.createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
