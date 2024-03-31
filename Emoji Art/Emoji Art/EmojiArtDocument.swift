ff9cfedfc48071b958b37e1e6412f5f8faa717bc
hanasiu <youryouu@naver.com>
2024-03-31 16:23:48 +0900

    lecture 10

--- Emoji Art/Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/Emoji Art/EmojiArtDocument.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     typealias Emoji = EmojiArt.Emoji
-    private var emojiArt = EmojiArt()
+    @Published private var emojiArt = EmojiArt()
+    
+    init() {
+        emojiArt.addEmoji("👩‍🦰", at: .init(x: -200, y:-150), size: 200)
+        emojiArt.addEmoji("🐼", at: .init(x: 250, y: 100), size: 80)
+    }
     
     var emojis: [Emoji] {
         emojiArt.emojis
@@ -39,6 +44,7 @@ extension EmojiArt.Emoji {
 
 extension EmojiArt.Emoji.Position {
     func `in`(_ geometry: GeometryProxy) -> CGPoint {
-        
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
     }
 }
--- Emoji Art/Emoji Art/Extensions.swift
+++ Emoji Art/Emoji Art/Extensions.swift
@@ -2,6 +2,15 @@
 
 import Foundation
 
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+    init(center: CGPoint, size: CGSize) {
+        self.init(origin: CGPoint(x: center.x-size.width/2, y: center.y-size.height/2), size: size)
+    }
+}
+
 extension String {
     var uniqued: String {
         reduce(into: "") { sofar, element in
