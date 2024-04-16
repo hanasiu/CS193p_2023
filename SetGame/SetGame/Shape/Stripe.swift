3ebe506e1baad2a58799ec0aac995f3e7548f122
hanasiu <youryouu@naver.com>
2024-04-16 14:02:49 +0900

    correct crush point

--- SetGame/SetGame/Shape/Stripe.swift
+++ SetGame/SetGame/Shape/Stripe.swift
@@ -2,16 +2,24 @@ import SwiftUI
 
 struct Stripe: Shape {
     func path(in rect: CGRect) -> Path {
-            var path = Path()
-            let width = rect.size.width
-            let height = rect.size.height
-            
-        for y in stride(from: 0, through: height, by: height / Constants.interval) {
-                path.move(to: CGPoint(x: 0, y: y))
-                path.addLine(to: CGPoint(x: width, y: y))
-            }
-        return path
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        
+        //        for y in stride(from: 0, through: height, by: height / Constants.interval) {
+        //                path.move(to: CGPoint(x: 0, y: y))
+        //                path.addLine(to: CGPoint(x: width, y: y))
+        //            }
+        
+        // Ensure the stride value is sensible, not less than 1
+        let strideValue = max(height / Constants.interval, 1)
+        
+        for y in stride(from: 0, through: height, by: strideValue) {
+            path.move(to: CGPoint(x: 0, y: y))
+            path.addLine(to: CGPoint(x: width, y: y))
         }
+        return path
+    }
     
     
     private struct Constants {
